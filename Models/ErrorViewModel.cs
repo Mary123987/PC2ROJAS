62f4bfa054a2bebcec10ce725f85ff65ef4cddaf
Mary Rojas <maryrojascordova08@gmail.com>
2024-09-29 01:32:44 -0500

    Archivos iniciales

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PC2ROJAS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+## Caso Práctico: Aplicación de Gestión de Cuentas Bancarias para un Banco en EE. UU.
